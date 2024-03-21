efdd9fcc380225d24488f5a7f364541627dc99a1
wizedev <brendow.inventor@gmail.com>
2024-03-20 21:58:08 -0300

    Pagina de Produto

--- produto/views.py
+++ produto/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Produto
 
 
 class Home(View):
     template_name = "produto/index.html"
 
     def get(self, request, *args, **kwargs):
-        return render(request,self.template_name)
+        produtos = Produto.objects.filter(is_published=True)
+        return render(request,self.template_name,context={'produtos':produtos})
